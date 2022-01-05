ac25619163b1c9a3cf354a80d603aa01f7538c5b
Akash B G <akashbpura@gmail.com>
2022-01-06 00:34:18 +0530

    Added to validate and test the first name

---
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistrationMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.UserRegistration;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter Your First Name: ");
+		String firstName = scanner.next();
+		
+		UserRegistration registration = new UserRegistration();
+		registration.validateFirstName(firstName);
+	}
+}
---
+++ src/test/java/com/bridgelabz/UserRegistration/UserDetailsTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.UserRegistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserDetailsTest {
+	
+	@Test
+	public void firstName_WhenCorrect_ShouldReturnTrue() {
+		UserRegistration register = new UserRegistration();
+		boolean isValid = register.validateFirstName("Rocky");
+		Assert.assertTrue(isValid);
+	}
+	
+	@Test
+	public void firstName_WhenSmall_ShouldReturnFalse() {
+		UserRegistration register = new UserRegistration();
+		boolean isValid = register.validateFirstName("ro");
+		Assert.assertFalse(isValid);
+	}
+	
+	@Test
+	public void firstName_WhenWrong_ShouldReturnFalse() {
+		UserRegistration register = new UserRegistration();
+		boolean isValid = register.validateFirstName("rocky");
+		Assert.assertFalse(isValid);
+	}
+}
